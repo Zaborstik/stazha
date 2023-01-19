f3018e62dd07f37e71a948c5a2d7bc72be7db3f8
Zaborstik <zaborstik@gmail.com>
2023-01-19 16:33:55 +0300

    modded

--- src/Main.java
+++ src/Main.java
@@ -12,13 +12,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        mergeNums("/Users/zaborstik/Desktop/testsint/in1.txt", "/Users/zaborstik/Desktop/testsint/in2.txt", true, true);
+        mergeNums("/Users/zaborstik/Desktop/testsstr/in1.txt", "/Users/zaborstik/Desktop/testsstr/in2.txt", false, false);
     }
 
     public static void mergeNums(String pathFile1, String pathFile2, boolean discend, boolean isNum){
         //isDiscendng по возрастанию - false, по убыванию true
 
-        //путь для сохранения out.txt
         Path pathOut = Path.of(pathFile1).getParent().resolve("out.txt");
         if (!Files.exists(pathOut)){
             try {
@@ -32,7 +31,6 @@ public class Main {
              Scanner file2 = new Scanner(new FileInputStream(pathFile2));
              FileWriter fileWriter = new FileWriter(pathOut.toFile())) {
 
-            //объединяем массивы, в случае, если массив не отсортирован, выполняем cортировку
             if (isNum){
                 mergeInnerInt(discend, file1, file2, fileWriter);
             } else {
@@ -41,7 +39,8 @@ public class Main {
 
 
         } catch (FileSystemException e){
-            System.out.println("Файл поврежден, вероятнее всего отусвет сортировка");
+            System.out.println("Файл поврежден, отусвет сортировка");
+
         }catch (IOException e) {
             System.out.println("Файл не читается");
         }catch (Exception e){
@@ -52,7 +51,6 @@ public class Main {
     private static void mergeInnerString(boolean discend, Scanner file1, Scanner file2, FileWriter fileWriter) throws IOException {
         HashMap<Integer, String> hashMap = new HashMap<> ();
 
-        //переменные буфера
         String tmp1 = file1.nextLine();
         String tmp2 = file2.nextLine();
 
@@ -62,21 +60,27 @@ public class Main {
 
         while (file1.hasNext() && file2.hasNext()){
             if ((tmp2.compareTo(tmp1) * (discend ? -1 : 1)) < 0){
-                if ((!hashMap.get(2).equals("")) && ((hashMap.get(2).hashCode() * (discend ? -1 : 1)) > (tmp2.hashCode() * (discend ? -1 : 1)))){
+                if ((!tmp2.contains("Ё"))
+                        &&(!hashMap.get(2).equals(""))
+                        && ((hashMap.get(2).hashCode() * (discend ? -1 : 1)) > (tmp2.hashCode() * (discend ? -1 : 1)))){
                     throw new FileSystemException("Отсуствует сортировка");
                 } else {
-                    hashMap.put(2, tmp2);
+                    if (!tmp2.contains("ё")) {
+                        hashMap.put(2, tmp2);
+                    }
                 }
-
                 fileWriter.write((tmp2) + "\n");
                 tmp2 = file2.nextLine();
             } else {
-                if ((!hashMap.get(1).equals("")) && ((hashMap.get(1).hashCode()* (discend ? -1 : 1)) > (tmp1.hashCode()) * (discend ? -1 : 1))){
+                if ((!tmp1.contains("Ё"))
+                        && (!hashMap.get(1).equals(""))
+                        && ((hashMap.get(1).hashCode()* (discend ? -1 : 1)) > (tmp1.hashCode()) * (discend ? -1 : 1))){
                     throw new FileSystemException("Отсуствует сортировка");
                 } else {
-                    hashMap.put(1, tmp1);
+                    if (!tmp1.contains("ё")){
+                        hashMap.put(1, tmp1);
+                    }
                 }
-
                 fileWriter.write((tmp1 + "\n"));
                 tmp1 = file1.nextLine();
             }
@@ -112,65 +116,63 @@ public class Main {
     private static void mergeInnerInt(boolean discend, Scanner file1, Scanner file2, FileWriter fileWriter) throws IOException {
         HashMap<Integer, Integer> hashMap = new HashMap<>();
 
-        //переменные буфера
-        int num1 = file1.nextInt() * (discend ? -1 : 1);
-        int num2 = file2.nextInt() * (discend ? -1 : 1);
+        int tmp1 = file1.nextInt() * (discend ? -1 : 1);
+        int tmp2 = file2.nextInt() * (discend ? -1 : 1);
 
         //кладем в мапу, чтобы проверять, отсортирован ли данный массив
-        hashMap.put(1, num1);
-        hashMap.put(2, num2);
+        hashMap.put(1, tmp1);
+        hashMap.put(2, tmp2);
 
         while (file1.hasNext() && file2.hasNext()){
-            if (num1 > num2){
-                if (hashMap.get(2) > num2){
-                    System.out.println("warn");
-                    //обработка неотсортированного массива
-                    return;
+            if (tmp1 > tmp2){
+                if (hashMap.get(2) > tmp2){
+                    throw new FileSystemException("Отсуствует сортировка");
                 } else {
-                    hashMap.put(2, num2);
+                    hashMap.put(2, tmp2);
                 }
-
-                fileWriter.write((num2 * (discend ? -1 : 1)) + "\n");
-                num2 = file2.nextInt() * (discend ? -1 : 1);
+                fileWriter.write((tmp2 * (discend ? -1 : 1)) + "\n");
+                tmp2 = file2.nextInt() * (discend ? -1 : 1);
             } else {
-                if (hashMap.get(1) > num1){
-                    System.out.println("warn");
-                    //обработка неотсортированного массива
-                    return;
+                if (hashMap.get(1) > tmp1){
+                    throw new FileSystemException("Отсуствует сортировка");
                 } else {
-                    hashMap.put(1, num1);
+                    hashMap.put(1, tmp1);
                 }
-
-                fileWriter.write((num1 * (discend ? -1 : 1)) + "\n");
-                num1 = file1.nextInt() * (discend ? -1 : 1);
+                fileWriter.write((tmp1 * (discend ? -1 : 1)) + "\n");
+                tmp1 = file1.nextInt() * (discend ? -1 : 1);
             }
         }
 
         //в одном из массивов закончились элементы
         if (file2.hasNext()) {
-            mergeOfOneInnerInt(discend, file2, fileWriter, num1, num2);
+            mergeOfOneInnerInt(discend, file2, fileWriter, tmp1, tmp2);
         }
         if (file1.hasNext()) {
-            mergeOfOneInnerInt(discend, file1, fileWriter, num2, num1);
+            mergeOfOneInnerInt(discend, file1, fileWriter, tmp2, tmp1);
         }
     }
 
-    private static void mergeOfOneInnerInt(boolean discend, Scanner file, FileWriter fileWriter, int lastElement, int num2) throws IOException {
-        //записан ли последний символ кратчайщего массива (костыли)
-        boolean written = false;
+    private static void mergeOfOneInnerInt(boolean discend, Scanner file, FileWriter fileWriter, int lastElement, int tmp2) throws IOException {
+        boolean written = false;        //записан ли последний символ кратчайщего массива (костыли)
+
 
         while (file.hasNext()){
-            if (lastElement >= num2 || written) {
-                fileWriter.write((num2 * (discend ? -1 : 1)) + "\n");
-                num2 = file.nextInt() * (discend ? -1 : 1);
+            if (lastElement >= tmp2 || written) {
+                fileWriter.write((tmp2 * (discend ? -1 : 1)) + "\n");
+                tmp2 = file.nextInt() * (discend ? -1 : 1);
             } else if (!written) {
                 fileWriter.write(lastElement * (discend ? -1 : 1) + "\n");
                 written = true;
             }
         } // проверить как с str
         if (!written){
-            fileWriter.write("" + (Math.min(lastElement,num2)) * (discend ? -1 : 1) + "\n");
+            fileWriter.write("" + (Math.min(lastElement,tmp2)) * (discend ? -1 : 1) + "\n");
         }
-        fileWriter.write("" + (Math.max(lastElement,num2)) * (discend ? -1 : 1));
+        fileWriter.write("" + (Math.max(lastElement,tmp2)) * (discend ? -1 : 1));
+    }
+
+    private static void sort(boolean discend, Scanner file){
+        String[] strings = new String[65536];
+
     }
 }
--- src/еуче2.java
+++ src/еуче2.java
@@ -2,10 +2,37 @@ package src;
 
 public class еуче2 {
     public static void main(String[] args) {
-        String str = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
-        char[] chars = str.toCharArray();
-        for (char c : chars) {
-            System.out.println(String.valueOf(c).hashCode());
+
+//        String str = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+//        char[] chars = str.toCharArray();
+//        for (char c : chars) {
+//            System.out.println(String.valueOf(c) + String.valueOf(c).hashCode());
+//        }
+//
+//        if (str.contains("ё")){
+//            System.out.println("ё".hashCode());
+//            System.out.println(str.replace("ё", "е"));
+//        }
+    }
+    public static String[] merge( String[] leftPart, String[] rightPart ) {
+        int cursorLeft = 0, cursorRight = 0, counter = 0;
+        String[] merged = new String[leftPart.length + rightPart.length];
+        while ( cursorLeft < leftPart.length && cursorRight < rightPart.length ) {
+            if (leftPart[cursorLeft].compareTo(rightPart[cursorRight] ) < 0 ) {
+                merged[counter] = leftPart[cursorLeft];
+                cursorLeft+=1;
+            } else {
+                merged[counter] = rightPart[cursorRight];
+                cursorRight+=1;
+            }
+            counter++;
         }
+        if ( cursorLeft < leftPart.length ) {
+            System.arraycopy( leftPart, cursorLeft, merged, counter, merged.length - counter );
+        }
+        if ( cursorRight < rightPart.length ) {
+            System.arraycopy( rightPart, cursorRight, merged, counter, merged.length - counter );
+        }
+        return merged;
     }
 }
